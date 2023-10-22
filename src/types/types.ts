cf856cf7a5b63a04064138c445b4b89b535f7838
Wheelyhog <wheelyhog@gmail.com>
2023-10-22 10:42:09 +0200

    add output of filtered products

---
+++ src/types/types.ts
@@ -0,0 +1,17 @@
+export type MenuType = {
+  id: string;
+  slug: string;
+  title: string;
+  desc?: string;
+  img?: string;
+  color: string;
+}[];
+
+export type ProductType = {
+  id: string;
+  slug: string;
+  title: string;
+  desc?: string;
+  img?: string;
+  color: string;
+}[];
